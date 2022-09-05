8ea4cb083caac3da81da133b75ae36a8ffb79aa2
Sævar Öfjörð Magnússon <saevar@saevar.is>
2022-09-05 14:27:20 +0000

    update

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,7 +1,3 @@
-Version 0.3
-===========
-* Added support for Wagtail 4.0
-
 Version 0.2
 ===========
 * Added support for Wagtail 3.0
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ documentation_extras = [
 
 setup(
     name='wagtailcharts',
-    version='0.3',
+    version='0.2',
     packages=['wagtailcharts'],
     include_package_data=True,
     license='MIT',
