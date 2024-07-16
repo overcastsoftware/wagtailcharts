05bf49e9c1d47db4cf2f827d40ae277cea7714aa
Jake Howard <git@theorangeone.net>
2024-07-16 12:21:53 +0100

    Convert render_charts to simple tag
    This allows the tag to be called easily from jinja2

--- wagtailcharts/templatetags/wagtailcharts_tags.py
+++ wagtailcharts/templatetags/wagtailcharts_tags.py
@@ -1,10 +1,11 @@
 from django import template
+from django.template.loader import render_to_string
 
 register = template.Library()
 
-@register.inclusion_tag('wagtailcharts/tags/render_charts.html')
-def render_charts():
-    pass
+@register.simple_tag(takes_context=True)
+def render_charts(context):
+    return render_to_string('wagtailcharts/tags/render_charts.html', context=context)
 
 @register.filter()
 def addspace(val):
