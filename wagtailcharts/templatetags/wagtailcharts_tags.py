ac276b96ce3ec0043eb2013f1663163a8afa3e5e
Sævar Öfjörð Magnússon <saevar@saevar.is>
2024-09-18 15:10:48 +0000

    Fix request context error as reported by #19

--- wagtailcharts/templatetags/wagtailcharts_tags.py
+++ wagtailcharts/templatetags/wagtailcharts_tags.py
@@ -3,9 +3,9 @@ from django.template.loader import render_to_string
 
 register = template.Library()
 
-@register.simple_tag(takes_context=True)
-def render_charts(context):
-    return render_to_string('wagtailcharts/tags/render_charts.html', context=context)
+@register.simple_tag
+def render_charts():
+    return render_to_string('wagtailcharts/tags/render_charts.html')
 
 @register.filter()
 def addspace(val):
